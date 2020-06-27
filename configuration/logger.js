72712ae8ef8e2bc483acb240dd098ef224c1b405
Alexis Torreblanca <alexis_torreblanca@outlook.com>
2020-06-27 01:54:30 -0500

    Added detailed console logging

--- configuration/logger.js
+++ configuration/logger.js
@@ -1,5 +1,17 @@
+//TODO: 
+// * Store logs in a file.
+//      * Divide logs by type in different files.
+// * Use Mexico Timestamp.
 const winston = require('winston');
+
+const loggerFormat = winston.format.combine(
+    winston.format.colorize(),
+    winston.format.timestamp(),
+    winston.format.printf(info => `[${info.timestamp}] ${info.level}: ${info.message}}`)
+);
+
 const logger = winston.createLogger({
+    format: loggerFormat,
     transports: [
         new winston.transports.Console()
     ]
