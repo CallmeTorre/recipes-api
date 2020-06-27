7fe59f57331ca82e66bc61450e8aebe983793066
Alexis Torreblanca <alexis_torreblanca@outlook.com>
2020-06-26 21:58:48 -0500

    Added cors and env vars

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-var express = require('express');
-var app = express();
+require('dotenv').config()
+const bodyParser = require('body-parser');
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-app.get('/', function (req, res) {
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.post('/', (req, res) => {
+  console.log(req);
+  res.send('Thanks');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}!`);
 });
